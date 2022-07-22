174cd4ecf1c4b515d5ebd532889e563fecbd67de
Adrien Tirlemont <ssquerel@gmail.com>
2022-07-22 16:42:10 +0200

    Back to BlogPost

--- Client/Program.cs
+++ Client/Program.cs
@@ -10,6 +10,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<IBlogService,BlogService2>(); 
+builder.Services.AddScoped<IBlogService,BlogService>(); 
 
 await builder.Build().RunAsync();
